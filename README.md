f1f27dc5f6107d051a1a8354fac8461947ff444d
mlu7 <815341194@qq.com>
2024-12-14 22:17:33 +0800

    choose telescope as default instead of fzf-lua

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+
+[2024 Dec 14]
+- fzf and tree-sitter need a C/cpp env. we can use Powershell+VS or mingw.
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,11 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.editor.fzf" },
+	
+	-- choose telescope as my default
+    { import = "lazyvim.plugins.extras.editor.fzf" ,enabled =false },
+	{ import = "lazyvim.plugins.extras.editor.telescope" },
+	
     -- import/override with your plugins
     { import = "plugins" },
   },
